1bc461bffd5434faa3e7f1d71bc4e609f4b85320
Daniel Lanner <hello@daniellanner.io>
2024-06-21 08:36:15 -0700

    Fix platform dependent invocation of sed

--- ci/social.bash
+++ ci/social.bash
@@ -7,7 +7,14 @@ pattern='<meta  property="og:image" .* >'
 replacement='<meta  property="og:image"  content="https://opencollaboration.io/assets/social.png" >'
 
 # replace generated social cards with static image recursively in all files
-grep -rl '<meta  property="og:image"' site | xargs sed -i '' "s|${pattern}|${replacement}|g"
+# check for platform for proper invocation of sed
+# macos expects temp file location for in place file changes
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    grep -rl '<meta  property="og:image"' site | xargs sed -i '' "s|${pattern}|${replacement}|g"
+else
+    grep -rl '<meta  property="og:image"' site | xargs sed -i "s|${pattern}|${replacement}|g"
+fi
+
 
 ###
 ### Replace twitter social cards
@@ -16,4 +23,10 @@ pattern='<meta  name="twitter:image" .* >'
 replacement='<meta  name="twitter:image"  content="https://opencollaboration.io/assets/social.png" >'
 
 # replace generated social cards with static image recursively in all files
-grep -rl '<meta  name="twitter:image"' site | xargs sed -i '' "s|${pattern}|${replacement}|g"
+# check for platform for proper invocation of sed
+# macos expects temp file location for in place file changes
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    grep -rl '<meta  name="twitter:image"' site | xargs sed -i '' "s|${pattern}|${replacement}|g"
+else
+    grep -rl '<meta  name="twitter:image"' site | xargs sed -i "s|${pattern}|${replacement}|g"
+fi
